a571a9c0ae91e7f07f3fa13682fd0b4498a10f59
nathanstac <101220128+nathanstac@users.noreply.github.com>
2022-03-10 09:18:13 +1300

    finished code

---
+++ raffle.1.py
@@ -0,0 +1,23 @@
+''' Raffle program '''
+
+# Set up list to store names of entrants
+names = []
+import random
+keep_asking = True
+print("welcome to the raffle program")
+print("what is the prize being raffled")
+prize = input()
+print(f"what is the value of {prize} ")
+value = int(input())
+
+while keep_asking == True:
+    print("enter the name of entrant")
+    name = input()
+    if name.lower() == "end":
+        keep_asking = False
+    else:
+        names.append(name)
+print(names)
+print("these are the people who have entered for the rafffle")
+winner = random.randint(0,len(names)-1)
+print(f"Winner of the {prize}, valued at ${value} is .... {names[winner]}")
